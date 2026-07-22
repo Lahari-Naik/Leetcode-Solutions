3bf00a609f0e9c4c3d1835c905b35deef88af192
Lahari_naik <182788277+Lahari-Naik@users.noreply.github.com>
2026-07-22 21:25:07 +0530

    Time: 49 ms (93.55%), Space: 255.5 MB (90.32%) - LeetHub

--- 3501-maximize-active-section-with-trade-ii/3501-maximize-active-section-with-trade-ii.cpp
+++ 3501-maximize-active-section-with-trade-ii/3501-maximize-active-section-with-trade-ii.cpp
@@ -1,115 +1,58 @@
-class SegmentTree {
-private:
-    int n;
-    vector<int> arr;
-    vector<int> seg;
+const int K = 17, MAXN = 1e5;
+int st[K + 1][MAXN];
 
-    void build(int p, int l, int r) {
-        if (l == r) {
-            seg[p] = arr[l];
-            return;
-        }
+void build(auto& array) {
+	copy(array.begin(), array.end(), st[0]);
 
-        int mid = (l + r) >> 1;
-        build(p << 1, l, mid);
-        build(p << 1 | 1, mid + 1, r);
-        seg[p] = max(seg[p << 1], seg[p << 1 | 1]);
-    }
+	for(int i = 1; i <= K; i++)
+	    for(int j = 0; j + (1 << i) <= array.size(); j++)
+	        st[i][j] = max(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+}
 
-    int _query(int p, int l, int r, int L, int R) {
-        if (L <= l && r <= R) {
-            return seg[p];
-        }
-
-        int mid = (l + r) >> 1;
-        int res = 0;
-        if (L <= mid) {
-            res = max(res, _query(p << 1, l, mid, L, R));
-        }
-        if (R > mid) {
-            res = max(res, _query(p << 1 | 1, mid + 1, r, L, R));
-        }
-
-        return res;
-    }
-
-public:
-    SegmentTree(const vector<int>& arr) : arr(arr) {
-        n = arr.size();
-        seg.resize(n << 2, 0);
-        build(1, 0, n - 1);
-    }
-
-    int query(int L, int R) {
-        if (L > R) {
-            return 0;
-        }
-
-        return _query(1, 0, n - 1, L, R);
-    }
-};
+int query(int L, int R) {
+	int i = bit_width(unsigned(R - L + 1)) - 1;
+	return max(st[i][L], st[i][R - (1 << i) + 1]);
+}
 
 class Solution {
 public:
-    vector<int> maxActiveSectionsAfterTrade(string s,
-                                            vector<vector<int>>& queries) {
-        int n = s.length();
-        int cnt1 = count(s.begin(), s.end(), '1');
-
-        vector<int> zeroBlocks;
-        vector<int> blockLeft;
-        vector<int> blockRight;
-
-        int i = 0;
-        while (i < n) {
-            int st = i;
-            while (i < n && s[i] == s[st]) {
-                i += 1;
-            }
-            if (s[st] == '0') {
-                zeroBlocks.push_back(i - st);
-                blockLeft.push_back(st);
-                blockRight.push_back(i - 1);
-            }
+    vector<int> maxActiveSectionsAfterTrade(const string& s, vector<vector<int>>& queries) {
+        int n = s.size(), active = 0;
+        vector<pair<int, int>> zero;
+        vector<int> index(n);
+        for(int i = 0; i < n; i++) {
+        	if(s[i] == '0') {
+        		if(i > 0 && s[i - 1] == '0') zero.back().second++;
+        		else zero.push_back({i, 1});
+        	}else {
+        		active++;
+        	}
+        	index[i] = int(zero.size()) - 1;
         }
-
-        int m = zeroBlocks.size();
-        if (m < 2) {
-            return vector<int>(queries.size(), cnt1);
+        if(zero.empty()) return vector<int>(queries.size(), active);
+        
+        vector<int> gains(zero.size() - 1);
+        for(int i = zero.size() - 2; i >= 0; i--) {
+        	gains[i] = zero[i].second + zero[i + 1].second;
         }
+        build(gains);
 
-        vector<int> tmpSum(m - 1);
-        for (int i = 0; i < m - 1; i++) {
-            tmpSum[i] = zeroBlocks[i] + zeroBlocks[i + 1];
+        vector<int> res(queries.size(), active);
+        for(int i = 0, sz = queries.size(); i < queries.size(); i++) {
+        	int L = queries[i][0], R = queries[i][1];
+        	int start = index[L] + 1, end = index[R] - (s[R] == '0');
+        	int cnt_left = index[L] == -1 ? -1 : (zero[index[L]].second - (L - zero[index[L]].first));
+    		int cnt_right = index[R] == -1 ? -1 : (R - zero[index[R]].first + 1);
+
+        	if(start < end)
+    			res[i] = max(res[i], active + query(start, end - 1));
+    		if(s[L] == '0' && s[R] == '0' && index[L] + 1 == index[R])
+    			res[i] = max(res[i], active + cnt_left + cnt_right);
+            if(s[L] == '0' && index[L] + 1 < index[R] + (s[R] == '1'))
+                res[i] = max(res[i], active + cnt_left + zero[index[L] + 1].second);
+            if(s[R] == '0' && index[L] < index[R] - 1)
+                res[i] = max(res[i], active + cnt_right + zero[index[R] - 1].second);
         }
-        SegmentTree seg(tmpSum);
-        vector<int> ans;
-
-        for (const auto& q : queries) {
-            int l = q[0], r = q[1];
-            int i = lower_bound(blockRight.begin(), blockRight.end(), l) -
-                    blockRight.begin();
-            int j = upper_bound(blockLeft.begin(), blockLeft.end(), r) -
-                    blockLeft.begin() - 1;
-            if (i > m - 1 || j < 0 || i >= j) {
-                ans.push_back(cnt1);
-                continue;
-            }
-            int firstLen = blockRight[i] - max(blockLeft[i], l) + 1;
-            int lastLen = min(blockRight[j], r) - blockLeft[j] + 1;
-            if (i + 1 == j) {
-                int bestGain = firstLen + lastLen;
-                ans.push_back(cnt1 + bestGain);
-                continue;
-            }
-
-            int val1 = firstLen + zeroBlocks[i + 1];
-            int val2 = zeroBlocks[j - 1] + lastLen;
-            int val3 = seg.query(i + 1, j - 2);
-            int bestGain = max({val1, val2, val3});
-            ans.push_back(cnt1 + bestGain);
-        }
-
-        return ans;
+        return res;
     }
 };
