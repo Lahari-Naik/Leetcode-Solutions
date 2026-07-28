3251610303059ffc82c3080e516017c5b59fe3b7
Lahari_naik <182788277+Lahari-Naik@users.noreply.github.com>
2026-07-28 13:50:01 +0530

    Time: 0 ms (100%), Space: 12.4 MB (25.56%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int hIndex(vector<int>& c) {
+        sort(c.begin(),c.end(),[](int a, int b)
+        {
+            return a > b;
+        });
+        if(c[0]==0) return 0;
+        int n = c.size();
+        int ans = 0;
+        int mini = INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            mini = min(i+1,c[i]);
+            ans = max(ans,mini);
+        }
+        return ans == 0 ? n : ans;
+    }
+};
